1c20832cdf2c77868193e9d7739d21eab70c0fff
Daniel Fróes <danielfroes15@gmail.com>
2020-04-29 17:57:24 -0300

    começo da documentação

--- clientChat.cpp
+++ clientChat.cpp
@@ -13,21 +13,27 @@
 #define MAX_MSG_LENGTH 4096
 #define USERNAME_LENGTH 100
 
-void readMessage(int sock)
+
+//func readMessage: Lê uma mensagem que foi enviada para o cliente
+//@param: socket do cliente
+
+void ReadMessage(int sock)
 {
     char buffer[MAX_MSG_LENGTH + USERNAME_LENGTH];
-    int valread;
+    int msgLength;
 
     while (true)
     {
-        valread = read(sock, buffer, MAX_MSG_LENGTH + USERNAME_LENGTH);
-        buffer[valread] = '\0';
+        msgLength = read(sock, buffer, MAX_MSG_LENGTH + USERNAME_LENGTH);
+        buffer[msgLength] = '\0';
         printf("%s", buffer);
     }
 
 }
 
-void sendMessage(std::string userName, int sock)
+//func readMessage: Lê uma mensagem que foi enviada para o cliente
+//@param: socket do cliente
+void SendMessage(std::string userName, int sock)
 {
     std::string msg;
     std::string msgAux;
@@ -49,19 +55,25 @@ void sendMessage(std::string userName, int sock)
     }
 }
 
-int main(int argc, char const *argv[])
-{
-    int sock = 0, valread;
-    struct sockaddr_in serv_addr;
-    char welcomeMsg[1000] = {0};
-    std::string userName;
 
-    //criando a socket do cliente
+int CreateSocket()
+{
+    int sock = 0;
+
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("\n Socket creation error \n");
-        return -1;
+        exit(-1);
     }
+    
+    return sock;
+}
+
+void ConnectToServer(int clientSock)
+{
+    int valread;
+    char welcomeMsg[1000] = {0};
+    struct sockaddr_in serv_addr;
 
     //configurando o endereço do servidor
     serv_addr.sin_family = AF_INET;
@@ -69,29 +81,41 @@ int main(int argc, char const *argv[])
     if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) // Convert IPv4 and IPv6 addresses from text to binary form
     {
         printf("\nInvalid address/ Address not supported \n");
-        return -1;
+        exit (-1);
     }
 
     //procura o servidor e conecta a socket
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    if (connect(clientSock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
         printf("\nConnection Failed \n");
-        return -1;
+        exit (-1);
     }
 
-    valread = read(sock, welcomeMsg, 1000);
+    valread = read(clientSock, welcomeMsg, 1000);
     printf("%s\n", welcomeMsg);
+}
+
+
+
+int main(int argc, char const *argv[])
+{
+    std::string userName;
+
+    //criando a socket do cliente
+    int clientSock = CreateSocket();
+    ConnectToServer(clientSock);
 
     printf("Digite um nome de usuário para entrar no chat: ");
     std::cin >> userName;
     std::cout << "\n\n Bem vindo ao chat, " << userName << "\n Para enviar sua mensagem basta digitar e apertar Enter\n\n";
 
-    std::thread readThread(readMessage, sock);
+    std::thread readThread(ReadMessage, clientSock);
 
-    std::thread sendThread(sendMessage, userName, sock);
+    std::thread sendThread(SendMessage, userName, clientSock);
 
     readThread.join();
     sendThread.join();
 
     return 0;
 }
+
