b8892202fbe7ec7619302694fceb886c2a0a8a23
Daniel Fróes <danielfroes15@gmail.com>
2020-04-29 15:51:35 -0300

    chat com threads funcionando

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 all:
-	gcc serverChat.c -o server.out
-	gcc clientChat.c -o client.out
+	g++ serverChat.cpp -o server.out
+	g++ clientChat.cpp -o client.out
 
-server: server.out serverChat.c
-	gcc serverChat.c -o server.out
+server: server.out serverChat.cpp
+	g++ serverChat.cpp -o server.out -std=c++11 -pthread
 	./server.out
 
-client: client.out clientChat.c
-	gcc clientChat.c -o client.out
-	./client.out
+client: client.out clientChat.cpp
+	g++ clientChat.cpp -o client.out -std=c++11 -pthread
+	./client.out 
--- clientChat.cpp
+++ clientChat.cpp
@@ -4,14 +4,53 @@
 #include <arpa/inet.h> 
 #include <unistd.h> 
 #include <string.h> 
+
+#include <iostream> 
+#include <thread> 
+
 #define PORT 8080 
    
+
+void readMessage(int sock)
+{
+    char buffer[1024];
+    int valread;
+
+    while(true)
+    {
+        valread = read(sock ,buffer, 1024);
+        buffer[valread] = '\0';
+        printf("%s\n",buffer );
+    }
+}
+
+void sendMessage( char* userName, int sock)
+{
+    char msg[1024];
+    char msgDest[1024];
+
+
+    strcat(userName, ": ");
+    while(true)
+    {
+        scanf("\n%[^\n]s", msg);
+
+        strcpy(msgDest,userName);
+        strcat(msgDest, msg);
+
+        send(sock , msgDest, strlen(msgDest) , 0 ); 
+    }
+}
+
+
 int main(int argc, char const *argv[]) 
 { 
     int sock = 0, valread; 
     struct sockaddr_in serv_addr; 
-    char msg[1024];
     char buffer[1024] = {0}; 
+    char userName[1024];
+
+    
 
     //criando a socket do cliente
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
@@ -19,7 +58,7 @@ int main(int argc, char const *argv[])
         printf("\n Socket creation error \n"); 
         return -1; 
     } 
-
+    
     //configurando o endereço do servidor
     serv_addr.sin_family = AF_INET; 
     serv_addr.sin_port = htons(PORT);    
@@ -38,21 +77,17 @@ int main(int argc, char const *argv[])
 
     valread = read(sock , buffer, 1024); 
     printf("%s\n",buffer );  
-    int opc;
-    char tmp[1024];
     
-    while(1){
-        scanf("%d", &opc);
-        if(opc == 1){
-            scanf("\n%[^\n]s", msg);
-            send(sock , msg , strlen(msg) , 0 ); 
-        }        
-        else{
-            valread = read(sock ,tmp, 1024);
-            tmp[valread] = '\0';
-            printf("%s\n",tmp );
-        }
-        
-    }
+    printf("Digite um nome de usuário para entrar no chat: ");
+    scanf("%s", userName);
+    printf("\n");
+
+    std::thread readThread(readMessage, sock);
+
+    std::thread sendThread(sendMessage, userName, sock);
+
+    readThread.join();
+    sendThread.join();
+
     return 0; 
 } 
